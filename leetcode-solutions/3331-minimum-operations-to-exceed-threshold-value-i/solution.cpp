74b6e345b42d39427cd9144abad273f941cf741b
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2024-09-04 18:33:14 +0000

    [LeetCode Sync] Runtime - 4 ms (1.63%), Memory - 24.6 MB (100.00%)

---
+++ leetcode-solutions/3331-minimum-operations-to-exceed-threshold-value-i/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) 
+    {
+        int count=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<k)
+            {
+                count++;
+            }
+            else
+            break;
+        }
+        return count;
+        
+    }
+};
