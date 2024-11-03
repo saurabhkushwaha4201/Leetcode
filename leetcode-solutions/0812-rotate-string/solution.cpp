d641c3529aa93e557bd4c0171fc2a8a7b1586a79
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2024-11-03 04:03:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.7 MB (100.00%)

---
+++ leetcode-solutions/0812-rotate-string/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) 
+    {
+        if(s.length()!=goal.length())
+        return false;
+        s=s+s;
+        return ( s.find(goal) != string::npos );
+    }
+};
