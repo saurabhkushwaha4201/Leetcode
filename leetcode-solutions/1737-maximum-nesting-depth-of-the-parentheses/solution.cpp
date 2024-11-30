63b27220937fda8d08f5f68a96ef9fdf170c6834
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2024-11-30 16:40:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.4 MB (24.48%)

---
+++ leetcode-solutions/1737-maximum-nesting-depth-of-the-parentheses/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxDepth(string s) 
+    {
+        int count=0;
+        int maxi = 0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='(')
+            count++;
+            else if(s[i]==')')
+            count--;
+            else
+            continue;
+
+            maxi=max(maxi,count);
+        }
+        return maxi;
+        
+    }
+};
