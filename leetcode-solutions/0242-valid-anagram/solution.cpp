157f11256c03c9421ce56e72f86f995cf2d9f4e6
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2024-12-16 13:59:48 +0000

    [LeetCode Sync] Runtime - 7 ms (20.58%), Memory - 9.7 MB (47.46%)

--- leetcode-solutions/0242-valid-anagram/solution.cpp
+++ leetcode-solutions/0242-valid-anagram/solution.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     bool isAnagram(string s, string t) 
     {
-        map<char,int>mpp1,mpp2;
-        for(int i=0;i<s.length();i++)
-        mpp1[s[i]]++;
-        for(int i=0;i<t.length();i++)
-        mpp2[t[i]]++;
-        return (mpp1==mpp2);
-        
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;        
     }
 };
