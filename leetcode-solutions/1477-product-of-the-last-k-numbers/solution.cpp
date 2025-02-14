aa0f3f59f9937f9205b6fb8848e219f7c3cf3ba5
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-02-14 05:40:25 +0000

    [LeetCode Sync] Runtime - 23 ms (72.90%), Memory - 77.9 MB (100.00%)

---
+++ leetcode-solutions/1477-product-of-the-last-k-numbers/solution.cpp
@@ -0,0 +1,49 @@
+class ProductOfNumbers {
+public:
+    int n;
+    vector<int>nums;
+    ProductOfNumbers() {
+        nums.clear();
+        n = 0;
+    }
+    
+    void add(int num) 
+    {
+        if(num==0)
+        {
+            nums = {};
+            n = 0;
+        }
+        else
+        {
+            if(nums.empty())
+            {
+                nums.push_back(num);
+            }
+            else
+            {
+                nums.push_back(num * nums[n-1]);
+            }
+            n++;
+        }
+        
+    }
+    
+    int getProduct(int k) {
+        if(k > n)
+        return 0;
+        else if(k==n)
+        return nums[n-1];
+        else
+        {
+            return  nums[n-1] / nums[n-k-1];
+        }
+    }
+};
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
