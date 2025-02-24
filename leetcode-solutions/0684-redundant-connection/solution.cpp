df108b38177d0e6f4d6e312a3c942d5bbfd9e5cf
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-02-24 17:25:29 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.7 MB (92.60%)

--- leetcode-solutions/0684-redundant-connection/solution.cpp
+++ leetcode-solutions/0684-redundant-connection/solution.cpp
@@ -1,43 +1,54 @@
 class Solution {
 public:
-    bool dfs(unordered_map<int,vector<int>>&adj,int u,int v,vector<bool>&visited)
-    {
-        visited[u] = true;
-        if(u==v)
-        return true;
+    vector<int> parent;
+    vector<int> rank;
 
-        for(auto &neighbour : adj[u])
-        {
-            if(visited[neighbour])
-            continue;
+    int find(int i) {
+        if (i == parent[i])
+            return i;
+        return parent[i] = find(parent[i]);
+    }
 
-            if(dfs(adj,neighbour,v,visited))
-            {
-                return true;
+    void unionByRank(int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent == y_parent) {
+            return;
+        }
+
+        if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;  // attach y_parent to x_parent
+        } else if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;  // attach x_parent to y_parent
+        } else {
+            parent[y_parent] = x_parent;  // arbitrarily attach y_parent to x_parent
+            rank[x_parent] += 1;           // increase rank of the new root
+        }
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        parent.resize(n + 1);
+        rank.resize(n + 1, 0);
+        
+        // Initialize parent of each node to itself.
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        vector<int> result;
+        for (int i = 0; i < n; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            if (find(u) != find(v)) {
+                unionByRank(u, v);
+            } else {
+                result = {u, v};  // Save the last redundant edge
             }
         }
-        return false;
-    }
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) 
-    {
-        
-
-        unordered_map<int,vector<int>>adj;
-
-        for(int i =0;i<edges.size();i++)
-        {
-            int u = edges[i][0];
-            int v  = edges[i][1];
-            vector<bool>visited(edges.size(),false);
-            if(adj.find(u)!=adj.end() && adj.find(v)!=adj.end()
-                && dfs(adj,u,v,visited))
-                {
-                    return edges[i];
-                }
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        return {};
-        
+        return result;
     }
 };
+
