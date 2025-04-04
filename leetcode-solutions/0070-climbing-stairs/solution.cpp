693de43db183e2db1003a8ad9e907258c3e793f6
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-04-04 02:24:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.9 MB (70.42%)

---
+++ leetcode-solutions/0070-climbing-stairs/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)
+        return n;
+        
+        int prev2 = 1,prev = 2,curr;
+
+        for(int i =3;i<=n;i++)
+        {
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return curr;
+    }
+};
