240d62f26dcf2f71874d3c25a36a03767be1a3ff
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-04-05 07:10:58 +0000

    [LeetCode Sync] Runtime - 8 ms (73.35%), Memory - 89.7 MB (94.36%)

--- leetcode-solutions/2888-minimum-index-of-a-valid-split/solution.cpp
+++ leetcode-solutions/2888-minimum-index-of-a-valid-split/solution.cpp
@@ -1,46 +1,50 @@
 class Solution {
 public:
-    int minimumIndex(vector<int>& nums) {
+    int minimumIndex(vector<int>& nums) 
+    {
         int n = nums.size();
-
-        int maj   = -1;
+        int maj = -1;
         int count = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(count == 0) {
-                maj = nums[i];
+        for(auto num:nums)
+        {
+            if(count==0)
+            {
+                maj = num;
                 count = 1;
-            } else if(nums[i] == maj) {
-                count++;
-            } else {
-                count--;
             }
+            else if(num==maj)
+            {
+                count++;
+            }
+            else
+            count--;
         }
 
         int majCount = 0;
-        for(int &num : nums) {
-            if(num == maj) {
+        for(auto num:nums)
+        {
+            if(num==maj)
+            {
                 majCount++;
             }
         }
-
         count = 0;
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == maj) {
+        for(int i =0;i<nums.size();i++)
+        {
+            if(nums[i]==maj)
+            {
                 count++;
             }
-
             int remainingCount = majCount - count;
+
             int n1 = i+1;
             int n2 = n-i-1;
-
-            if(count*2 > n1 && remainingCount*2 > n2) {
+            if(count > n1/2 && remainingCount > n2/2)
+            {
                 return i;
             }
         }
-
-
         return -1;
-
+        
     }
 };
