ea80b35a4ae742cb328cee9e76bb94578eadbb58
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-04-12 15:00:34 +0000

    [LeetCode Sync] Runtime - 2 ms (20.60%), Memory - 199.1 MB (8.15%)

---
+++ leetcode-solutions/3824-number-of-unique-xor-triplets-i/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        if(n<3)
+        {
+            return n;
+        }
+        int b = floor(log2(n)) + 1;
+        return 1<<b;
+    }
+};
