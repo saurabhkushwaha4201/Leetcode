72a2efe785189208d2c5a12d40b424321479fc76
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-04-15 17:56:59 +0000

    [LeetCode Sync] Runtime - 4 ms (6.85%), Memory - 13.2 MB (46.07%)

---
+++ leetcode-solutions/0120-triangle/solution.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int row;
+    // int solve(vector<vector<int>>& triangle, int i, int j, vector<vector<int>>&dp)
+    // {
+    //     if(i==row-1)
+    //     {
+    //         return triangle[i][j];
+    //     }
+    //     if(dp[i][j]!=-1)
+    //     return dp[i][j];
+    //     int down = triangle[i][j] + solve(triangle,i+1,j,dp);
+    //     int diagonaly = triangle[i][j] + solve(triangle,i+1,j+1,dp);
+
+    //     return dp[i][j] = min(down,diagonaly);
+    // }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        row = triangle.size();
+        // vector<vector<int>>dp(row,vector<int>(row,-1));
+        // vector<vector<int>>dp;
+        // dp = triangle;
+        // for (auto& row : dp) {
+        //     fill(row.begin(), row.end(), -1);
+        // }
+
+         // return solve(triangle,0,0,dp);
+
+        // Method - 2 Tabulation
+
+        // vector<vector<int>>dp;
+        // dp = triangle;
+        // for (auto& row : dp) {
+        //     fill(row.begin(), row.end(), 0);
+        // }
+        // for (int j = 0; j < row; j++) {
+        //     dp[row - 1][j] = triangle[row - 1][j];
+        // }
+
+        // for(int i = row-2;i>=0;i--)
+        // {
+        //     for(int j = 0;j<=i;j++)
+        //     {
+        //         int down = triangle[i][j] + dp[i+1][j];
+        //         int diagonal = triangle[i][j] + dp[i+1][j+1];
+        //         dp[i][j] = min(down,diagonal);
+        //     }
+        // }
+        // return dp[0][0];
+
+        // Method - 3 Space Optimisation
+
+        vector<int>prev(row);
+        for(int j = 0;j<row;j++)
+        {
+            prev[j] = triangle[row-1][j];
+        }
+
+        for(int i = row-2;i>=0;i--)
+        {
+            vector<int>curr(i+1);
+            for(int j = 0;j<=i;j++)
+            {
+                int down = triangle[i][j] + prev[j];
+                int diagonal = triangle[i][j] + prev[j+1];
+
+                curr[j] = min(down,diagonal);
+            }
+            prev = curr;
+        }
+        return prev[0];
+    }
+};
