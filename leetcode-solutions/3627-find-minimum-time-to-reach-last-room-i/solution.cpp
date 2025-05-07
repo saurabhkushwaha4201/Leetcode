008b6cf0a687a343e94ed8e018bc3ddf59d49f78
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-05-07 16:20:55 +0000

    [LeetCode Sync] Runtime - 19 ms (76.39%), Memory - 28.7 MB (95.71%)

---
+++ leetcode-solutions/3627-find-minimum-time-to-reach-last-room-i/solution.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    typedef pair<int,pair<int,int>>P;
+    vector<pair<int,int>>directions = {{-1,0},{1,0},{0,-1},{0,1}};
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int m = moveTime.size();
+        int n = moveTime[0].size();
+        vector<vector<int>>minTime(m,vector<int>(n,INT_MAX));
+        priority_queue<P,vector<P>,greater<P>>pq;
+
+        pq.push({0,{0,0}});
+        minTime[0][0] = 0;
+
+        while(!pq.empty())
+        {
+            int currTime = pq.top().first;
+            int currRow = pq.top().second.first;
+            int currCol = pq.top().second.second;
+            pq.pop();
+
+            if(currRow == m-1 && currCol == n-1)
+            return minTime[currRow][currCol];
+
+            for(auto dir:directions)
+            {
+                int nRow = currRow + dir.first;
+                int nCol = currCol + dir.second;
+
+                if(nRow >= 0 && nRow < m && nCol >= 0 && nCol < n)
+                {
+                    int waitTime  = max((moveTime[nRow][nCol] - currTime),0 );
+
+                    int newTime = waitTime + currTime + 1;
+                    if(minTime[nRow][nCol]>newTime)
+                    {
+                        minTime[nRow][nCol] = newTime;
+                        pq.push({newTime,{nRow,nCol}});
+                    }
+                }
+            }
+
+        }
+
+        return minTime[m-1][n-1];
+    }
+};
