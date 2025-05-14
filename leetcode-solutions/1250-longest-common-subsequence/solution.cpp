a9c7da3d457a0182049a5ee64886ab8c89ab8bfa
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-05-14 18:48:37 +0000

    [LeetCode Sync] Runtime - 14 ms (92.81%), Memory - 9 MB (96.51%)

--- leetcode-solutions/1250-longest-common-subsequence/solution.cpp
+++ leetcode-solutions/1250-longest-common-subsequence/solution.cpp
@@ -3,22 +3,23 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int len1 = text1.length();
         int len2 = text2.length();
-        vector<vector<int>>dp(len1+1,vector<int>(len2+1,0));
+        vector<int>prev(len2+1,0),curr(len2+1,0);
 
-        for(int i =1;i<=len1;i++)
+        for(int i = 1;i<=len1;i++)
         {
             for(int j = 1;j<=len2;j++)
             {
                 if(text1[i-1]==text2[j-1])
                 {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 }
                 else
                 {
-                    dp[i][j] = max(dp[i-1][j] ,dp[i][j-1]);
+                    curr[j] = max(prev[j],curr[j-1]);
                 }
             }
+            prev = curr;
         }
-        return dp[len1][len2];
+        return prev[len2];
     }
 };
