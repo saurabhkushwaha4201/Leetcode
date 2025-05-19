76039e34c598426b15b7e6c4529fdb82f355dd4d
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-05-19 17:28:50 +0000

    [LeetCode Sync] Runtime - 15 ms (79.71%), Memory - 9 MB (83.18%)

---
+++ leetcode-solutions/0115-distinct-subsequences/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+
+        vector<unsigned long long >prev(n+1,0),curr(n+1,0);
+
+        prev[0] = curr[0] = 1;
+        for(int i = 1;i<=m;i++)
+        {
+            for(int j = 1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    curr[j] = prev[j-1] + prev[j];
+                }
+                else
+                {
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr;
+        }
+        return  prev[n];
+    }
+};
