5390aa9887cf9e5bcf561626f448901998912935
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-05-27 14:39:06 +0000

    [LeetCode Sync] Runtime - 605 ms (6.23%), Memory - 296.4 MB (25.36%)

---
+++ leetcode-solutions/0300-longest-increasing-subsequence/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int index, int prev, vector<vector<int>>& dp)
+    {
+        if(index < 0)
+            return 0;
+
+        if(dp[index][prev+1] != -1)
+            return dp[index][prev+1];
+
+        int take = 0;
+        if(prev == -1 || nums[prev] > nums[index])
+        {
+            take = 1 + solve(nums, index - 1, index, dp);
+        }
+        int notTake = solve(nums, index - 1, prev, dp);
+
+        
+        return dp[index][prev+1] = max(take, notTake);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        // prev ranges from -1 to n-1, so size is n+1
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        return solve(nums, n-1, -1, dp);
+    }
+};
+
