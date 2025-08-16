8d5585fd487af0889aba532dd48edf2317566fce
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-08-16 02:18:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (93.90%)

---
+++ leetcode-solutions/1448-maximum-69-number/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string temp = to_string(num);
+        for(int i = 0;i<temp.length();i++)
+        {
+            if(temp[i]=='6')
+            {
+                temp[i] = '9';
+                break;
+            }
+        }
+        return stoi(temp);
+    }
+};
