cbf4ef35a1a0b45c6a9de33772ab883210be700c
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-09-19 16:44:29 +0000

    [LeetCode Sync] Runtime - 140 ms (13.20%), Memory - 191.5 MB (42.92%)

--- leetcode-solutions/3797-design-spreadsheet/solution.cpp
+++ leetcode-solutions/3797-design-spreadsheet/solution.cpp
@@ -1,42 +1,39 @@
 class Spreadsheet {
 public:
-    int totalRows;
-    unordered_map<std::string, int> cells;
-    Spreadsheet(int rows):totalRows(rows) {
+    vector<vector<int>>sheet;
 
+    Spreadsheet(int rows) {
+        sheet.assign(rows,vector<int>(26,0));
     }
     
     void setCell(string cell, int value) {
-        cells[cell] = value;
+        int row = stoi(cell.substr(1)) - 1;
+        int col = cell[0] - 'A';
+        sheet[row][col] = value;
     }
     
     void resetCell(string cell) {
-        cells[cell]= 0;
+        int row = stoi(cell.substr(1)) - 1;
+        int col = cell[0] - 'A';
+        sheet[row][col] = 0;
     }
-    
+    int solve(string s){
+        if(isdigit(s[0])){
+            return stoi(s);
+        }
+
+        int row = stoi(s.substr(1)) - 1;
+        int col = s[0] - 'A';
+        return sheet[row][col];
+    }
     int getValue(string formula) {
-        if (formula[0] != '=') {
-            return 0; // Formula must start with '='
-        }
+        string s = formula.substr(1);
+        int plusIdx = s.find('+');
 
-        size_t plusPos = formula.find('+');
-        if (plusPos == std::string::npos) {
-            return 0; // Formula must contain '+'
-        }
+        string left = s.substr(0,plusIdx);
+        string right = s.substr(plusIdx+1);
 
-        // Extract the two operands
-        string left = formula.substr(1, plusPos - 1);
-        string right = formula.substr(plusPos + 1);
-
-        // Trim spaces from the operands
-        left.erase(remove(left.begin(), left.end(), ' '), left.end());
-        right.erase(remove(right.begin(), right.end(), ' '), right.end());
-
-        // get values
-        int num1 = isalpha(left[0]) ? (cells.count(left) ? cells[left] : 0) : std::stoi(left);
-        int num2 = isalpha(right[0]) ? (cells.count(right) ? cells[right] : 0) : std::stoi(right);
-
-        return num1 + num2;
+        return solve(left) + solve(right);
     }
 };
 
