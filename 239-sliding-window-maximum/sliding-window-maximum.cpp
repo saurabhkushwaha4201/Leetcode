a6c51ab519d0365052aacec8d5ac33c71bffa33b
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-12-11 16:28:42 +0530

    Time: 29 ms (37.86%) | Memory: 139.3 MB (38.21%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int>result;
-        deque<int>dq;
+        deque<int> dq;     // stores window candidates in decreasing order
+        vector<int> ans;
         int n = nums.size();
-        for(int i = 0;i<n;i++){
-            // check for out of window
-            while(!dq.empty()&&dq.front()<=(i-k)){
+
+        for (int i = 0; i < n; i++) {
+
+            // 1. Remove old value from deque (value that slides out)
+            if (i >= k && !dq.empty() && dq.front() == nums[i - k]) {
                 dq.pop_front();
             }
-            while(!dq.empty()&& nums[dq.back()]<nums[i]){
+
+            // 2. Maintain decreasing order
+            while (!dq.empty() && dq.back() < nums[i]) {
                 dq.pop_back();
             }
-            // push el
-            dq.push_back(i);
-            // if window is equal size take max from front
-            if(i>=k-1){
-                result.push_back(nums[dq.front()]);
+
+            // 3. Push current value
+            dq.push_back(nums[i]);
+
+            // 4. Capture window max
+            if (i >= k - 1) {
+                ans.push_back(dq.front());
             }
         }
-        return result;
 
+        return ans;
     }
 };
