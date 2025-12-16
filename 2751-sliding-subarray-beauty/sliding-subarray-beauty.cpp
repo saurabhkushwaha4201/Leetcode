72c7e1daca2285d436d4d1c7e28f10c57c0e1f77
saurabhkushwaha4201 <147940138+saurabhkushwaha4201@users.noreply.github.com>
2025-12-16 14:29:22 +0530

    Time: 33 ms (88.52%) | Memory: 190.7 MB (78.02%) - LeetSync

--- 2751-sliding-subarray-beauty/sliding-subarray-beauty.cpp
+++ 2751-sliding-subarray-beauty/sliding-subarray-beauty.cpp
@@ -1,41 +1,32 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-
 class Solution {
 public:
-    typedef tree<
-        pair<long long, int>,
-        null_type,
-        less<pair<long long, int>>,
-        rb_tree_tag,
-        tree_order_statistics_node_update
-    > ordered_multiset;
-
+    int getBeauty(vector<int>&freq, int x){
+        int count = 0;
+        for(int val = 50;val>=1;val--){
+            count += freq[val];
+            if(count>=x){
+                return -val;
+            }
+        }
+        return 0;
+    }
     vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
-        ordered_multiset os;
-        vector<int> result;
         int n = nums.size();
-
-        for(int i = 0; i < n; i++) {
-            if(nums[i] < 0) {
-                os.insert({nums[i], i});
+        vector<int>freq(51,0),result;
+        for(int i = 0;i<k;i++){
+            if(nums[i]<0){
+                freq[-nums[i]]++;
             }
-
-            if(i >= k-1) {
-                if(os.size() < x) {
-                    result.push_back(0);
-                } else {
-                    int xth = os.find_by_order(x-1)->first; // corrected
-                    result.push_back(xth);
-                }
-
-                // erase outgoing element
-                if(nums[i-k+1] < 0) {
-                    auto it = os.find({nums[i-k+1], i-k+1}); // corrected
-                    if(it != os.end()) os.erase(it);
-                }
+        }
+        result.push_back(getBeauty(freq, x));
+        for(int i=k;i<n;i++){
+            if(nums[i-k]<0){
+                freq[-nums[i-k]]--;
             }
+            if(nums[i]<0){
+                freq[-nums[i]]++;
+            }
+            result.push_back(getBeauty(freq, x));
         }
         return result;
     }
